c2af9545148dd207f2f04b6a3d7011427a87c259
KaedeNakashima2 <ns-kaede.nakajima@nex.nikkei.com>
2020-05-04 07:29:53 +0900

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import Logo from './gameofthrones_logo.jpeg'
 import Actor1 from './sophia_turner.jpg'
-class App extends Component {
-  state = {
+
+const App = props => {
+  const [personsState, setPeronsState] = useState({
     persons: [
       { name: 'Emilia Clarke', actingName: 'Daenerys Targaryen' },
       { name: 'Maisie Williams', actingName: 'Arya Stark' },
       { name: 'Sophie Turner', actingName: 'Sansa Stark' },
     ],
     otherState: 'some other value'
-  }
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     //console.log('Was clicked!');
     // X this.state.persons[0].name = 'Katie2'
     this.setState({
@@ -22,24 +26,24 @@ class App extends Component {
         { name: 'Maisie Williams', actingName: 'Arya Stark' },
         { name: 'Sophie Turner', actingName: 'Sansa Stark' },
       ]
-    })
-  }
+    });
+  };
 
-  render() {
-    //jsx
-    return (
-      <div className='App'>
-        <img src={Logo} style={{ width: '30em' }} />
-        <h1>Main Cast</h1>
-        <p>Starring</p>
-        <button onClick={this.switchNameHandler}>See another cast</button>
-        <Person name={this.state.persons[0].name} actingName={this.state.persons[0].actingName} />
-        <Person name={this.state.persons[1].name} actingName={this.state.persons[1].actingName} />
-        <Person name={this.state.persons[2].name} actingName={this.state.persons[2].actingName}><img src={Actor1} /></Person>
-      </div>
-    );
-    //return React.createElement('div', {className: App }, React.createElement('h1', null, 'Dooes this work now?'))
-  }
+  console.log(personsState, otherState);
+
+  //jsx
+  return (
+    <div className='App'>
+      <img src={Logo} style={{ width: '30em' }} />
+      <h1>Main Cast</h1>
+      <p>Starring</p>
+      <button onClick={switchNameHandler}>See another cast</button>
+      <Person name={personsState.persons[0].name} actingName={personsState.persons[0].actingName} />
+      <Person name={personsState.persons[1].name} actingName={personsState.persons[1].actingName} />
+      <Person name={personsState.persons[2].name} actingName={personsState.persons[2].actingName}><img src={Actor1} /></Person>
+    </div>
+  );
+  //return React.createElement('div', {className: App }, React.createElement('h1', null, 'Dooes this work now?'))
 }
 
 export default App;
