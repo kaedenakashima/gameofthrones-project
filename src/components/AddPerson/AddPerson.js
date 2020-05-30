8033bdb480a624451a7abdc6fd8ebfa037a64ff6
kaedenakashima <ns-kaede.nakajima@nex.nikkei.com>
2020-05-30 14:17:31 +0900

    third commit

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <h4>2. Applicants</h4>
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends Component {
+    state = {
+        name: '',
+        age: ''
+    }
 
-export default addPerson;
+    nameChangedHandler = (event) => {
+        this.setState({ name: event.target.value });
+    }
+
+    ageChangedHandler = (event) => {
+        this.setState({ age: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="AddPerson">
+                <h4>2. Applicants</h4>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    onChange={this.nameChangedHandler}
+                    value={this.state.name}
+                />
+                <input
+                    type="number"
+                    placeholder="Age"
+                    onChange={this.ageChangedHandler}
+                    value={this.state.age}
+                />
+                <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+            </div>
+        );
+    }
+
+}
+
+export default AddPerson;
